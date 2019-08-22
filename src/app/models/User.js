0279d1fc8631b1fb3ce2478b5a506c35e20bf06f
Richard Yamamoto <rybolteri@gmail.com>
2019-08-22 16:40:14 -0300

    chore: Criando loader de models

---
+++ src/app/models/User.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default User;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!!a' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Richard Yamamoto',
+    email: 'rich@email',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
