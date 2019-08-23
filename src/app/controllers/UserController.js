674d2a3ccc9f60adc28aa25852651c2d5a00cf69
Richard Yamamoto <rybolteri@gmail.com>
2019-08-23 15:16:06 -0300

    chore: Autenticaçao e Middleware de autenticaçao

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -17,6 +17,7 @@ class UserController {
   }
 
   async update(req, res) {
+    console.log(req.userId);
     return res.json({ ok: true });
   }
 }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '295ce57763892728eac27bee079b9615',
+  expiresIn: '7d',
+};
