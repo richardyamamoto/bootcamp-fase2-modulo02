f9ce1e46a4f5062f4cd4902e092b70b01ea4f21d
Richard Yamamoto <rybolteri@gmail.com>
2019-08-24 19:44:17 -0300

    chore: File migration created

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,9 @@
+import multer from 'multer';
+
+class FileController {
+  async store(req, res) {
+    return res.json(req.file);
+  }
+}
+
+export default new FileController();
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,11 +16,7 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({
-    message: 'File uploaded',
-  });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 routes.put('/users', UserController.update);
 
 export default routes;
