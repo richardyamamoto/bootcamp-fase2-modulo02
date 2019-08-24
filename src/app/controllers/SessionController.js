d7d731a8764ad80c84b32f25af8089bdc24984e5
Richard Yamamoto <rybolteri@gmail.com>
2019-08-24 19:56:21 -0300

    fix: Password validation on session

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -24,8 +24,8 @@ class SessionController {
       return res.status(401).json({ error: 'User not found' });
     }
 
-    if (!user.checkPassword(password)) {
-      return res.status(401).json({ erros: 'Password does not match' });
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
     }
 
     const { id, name } = user;
