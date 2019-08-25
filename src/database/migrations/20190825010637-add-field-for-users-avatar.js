2601ac7c0bafee8027de3dbe690f24d53bfcfca4
Richard Yamamoto <rybolteri@gmail.com>
2019-08-25 17:47:56 -0300

    chore: Appointments migration and model

--- src/database/migrations/20190825010637-add-field-for-users-avatar.js
+++ src/database/migrations/20190825010637-add-field-for-users-avatar.js
@@ -2,17 +2,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'avatar_id', {
       type: Sequelize.INTEGER,
-      references: {
-        model: 'files',
-        key: 'id',
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
     });
   },
 
   down: queryInterface => {
-    return queryInterface.remove('users', 'avatar_id');
+    return queryInterface.removeColumn('users', 'avatar_id');
   },
 };
